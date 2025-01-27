568cb88fd49f1d173588ec1b1e84fb893dee5796
ifratul36 <jannatrituwd@gmail.com>
2025-01-28 05:13:02 +0600

    Created API Routes for Tour Package CRUD Operations

--- index.js
+++ index.js
@@ -273,9 +273,7 @@ async function run() {
     // });
     // app.delete("/candidates/:id", verifyToken, async (req, res) => {
     //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await candidatesCollection.deleteOne(query);
-    
+  
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
